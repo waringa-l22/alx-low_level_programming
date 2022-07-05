a0abdcc20a3c9dbb15560705cc53387e8111500a
waringa-l22 <lwaringa22@gmail.com>
2022-07-05 03:41:48 +0300

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
-putchar('');
+putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
@@ -25,7 +25,7 @@ if (num1 == 98 && num2 == 99)
 continue;
 
 putchar(',');
-putchar('');
+putchar(' ');
 }
 }
 putchar('\n');
