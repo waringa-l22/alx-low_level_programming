5da7bd4e76ffbfb50d6f4de14215e7151ba67c31
waringa-l22 <lwaringa22@gmail.com>
2022-07-07 01:51:30 +0300

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - function to print alphabet in lowercase 10 times
+ * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
  */
+
 void print_alphabet_x10(void)
 {
 	char c, i;
 :wq
-	for (i=0; i<=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (c='a'; c<='z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
