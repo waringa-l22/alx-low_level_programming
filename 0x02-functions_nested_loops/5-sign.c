fbc60a4c8e7e4253e274ac66c18846533ebf7f55
waringa-l22 <lwaringa22@gmail.com>
2022-07-07 00:48:30 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - function to print the sign of a number
+ * @n: is th eint that will be used for the argument of the function 
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+}
+	else if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+	else
+{
+	_putchar('0');
+	return (0);
+}
+}
