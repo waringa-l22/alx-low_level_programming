b8388c2b5e197608bbaad2b7b7a3459dd0133a64
waringa-l22 <lwaringa22@gmail.com>
2022-07-08 02:51:10 +0300

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c;
+
+	c= '0';
+:x
+
+	     c='a';
+	         printf("\nResult when non-numeric character is passed: %d", _isdigit(c));
+
+return 0;
+}
