0e028d484ca440fe8834dd4a343bdd717d019a9f
waringa-l22 <lwaringa22@gmail.com>
2022-07-08 03:42:41 +0300

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,13 @@
-#!
+#include "main.h"
+
+/**
+ * _isdigit - funtion to check for a digit
+ * @c: an integer argument
+ * Return: success
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
