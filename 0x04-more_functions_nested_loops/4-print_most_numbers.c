7e506aa6e8805413a8140bc52200d14fec0aa728
waringa-l22 <lwaringa22@gmail.com>
2022-07-08 03:54:44 +0300

    4 betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_most_numbers - finction that prints the numbers, from 0 to 9, followed by a new line
-*/ 
+ * print_most_numbers - finction that prints the numbers, from 0 to 9, followed by a new line
+ */ 
 
 void print_most_numbers(void)
 {
