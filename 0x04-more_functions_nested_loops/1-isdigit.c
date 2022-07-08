5ef1c67ad937b48c9b263e0ebfb0b4321731469b
waringa-l22 <lwaringa22@gmail.com>
2022-07-08 03:01:44 +0300

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,6 +3,7 @@
 
 /**
  * main - check the code
+ * _isdigit(int c) - function that checks for a digit 0 through 9
  *
  * Return: Always 0.
  */
@@ -12,9 +13,8 @@ int main(void)
 	char c;
 
 	c= '0';
-
-	     c='a';
-	         printf("\nResult when non-numeric character is passed: %d", _isdigit(c));
-
-return 0;
+	printf ("%c: %d\n", c, _isdigit(c));
+	c='a';
+	printf("%c : %d\n", c, _isdigit(c));
+	return (0);
 }
