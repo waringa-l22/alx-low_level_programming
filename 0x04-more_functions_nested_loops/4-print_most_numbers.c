5920ee0a17cc0bd146601f747253c6687e2a7191
waringa-l22 <lwaringa22@gmail.com>
2022-07-08 04:32:56 +0300

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,6 @@
 /**
 * print_most_numbers - function that prints the numbers, from 0 to 9, followed by a new line
 */ 
-
 void print_most_numbers(void)
 {
 int x;
@@ -11,7 +10,7 @@ int x;
 for (x = 0; x < 10; x++)
 {
 if (x != 2 && x != 4)
-_putchar(x + '0');
+	_putchar(x + '0');
 }
 _putchar('\n');
 }
