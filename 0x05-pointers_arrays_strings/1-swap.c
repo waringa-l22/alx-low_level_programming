a36a873a032e9e97ac47f0e7b72ebbddad743f12
waringa-l22 <lwaringa22@gmail.com>
2022-07-13 02:17:21 +0300

    task 1 and 5

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* swap_int - swaps the value of int a and int b
-* @a: first int to swap
-* @b: second int to swap
-*
-* Return: void 
-*/
+ * swap_int - swaps the value of int a and int b
+ * @a: first int to swap
+ * @b: second int to swap
+ *
+ * Return: void
+ */
 void swap_int(int *a, int *b)
 {
 int c;
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,5 +16,6 @@ void rev_string(char *s)
 	{
 		tmp = s[i];
 		s[i] = s[len - i - 1];
+		s[len - i -1] = tmp;
 	}
 }
