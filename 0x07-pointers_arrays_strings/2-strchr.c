1263d195a13c80524186b74c486a1d9c8f6a8579
waringa-l22 <lwaringa22@gmail.com>
2022-07-19 02:27:17 +0300

    still 2 and 9

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,3 @@
  * If c is not found - NULL.
  */
 
-char *_strchr(char *s, char c)
-{
-        int index;
-
-        for (index = 0; s[index] >= '\0'; index++)
-        {
-                if (s[index] == c)
-                        return (s + index);
-        }
-
-
-        return ('\0');
-}
