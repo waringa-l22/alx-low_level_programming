2eb1564bc6996b131da633ddca56d74546c7312f
waringa-l22 <107057287+waringa-l22@users.noreply.github.com>
2022-07-19 02:29:34 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,4 +8,16 @@
  * Return: If c is found - a pointer to the first occurence.
  * If c is not found - NULL.
  */
+char *_strchr(char *s, char c)
+{
+	int index;
 
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+
+	return ('\0');
+}
