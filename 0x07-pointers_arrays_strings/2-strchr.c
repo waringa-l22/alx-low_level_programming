fbd66bfd82859108e0f72719b6de84fa6ed9e8b1
waringa-l22 <lwaringa22@gmail.com>
2022-07-19 01:04:09 +0300

    Task on strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,15 @@
  * @s: The string to be searched.
  * @c: The character to be located.
  *
- * Return: A pointer to the first occurrence of the character c in the string.
+ * Return: If c is found - a pointer to the first occurence.
  * If c is not found - NULL.
  */
+
 char *_strchr(char *s, char c)
 {
         int index;
 
-        for (index = 0; s[index] > '\0'; index++)
+        for (index = 0; s[index] >= '\0'; index++)
         {
                 if (s[index] == c)
                         return (s + index);
