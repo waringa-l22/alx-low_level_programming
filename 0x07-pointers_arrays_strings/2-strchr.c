cfc08af90d574d2b85a64921b0178907e28bc247
waringa-l22 <lwaringa22@gmail.com>
2022-07-19 00:45:51 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,14 @@
  * @s: The string to be searched.
  * @c: The character to be located.
  *
- * Return: If c is found - a pointer to the first occurence.
+ * Return: A pointer to the first occurrence of the character c in the string.
  * If c is not found - NULL.
  */
 char *_strchr(char *s, char c)
 {
         int index;
 
-        for (index = 0; s[index] >= '\0'; index++)
+        for (index = 0; s[index] > '\0'; index++)
         {
                 if (s[index] == c)
                         return (s + index);
