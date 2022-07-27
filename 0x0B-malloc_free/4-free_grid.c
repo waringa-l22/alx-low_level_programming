c63bf365e44712b0d675e65657904b92537712c9
waringa-l22 <lwaringa22@gmail.com>
2022-07-27 04:32:06 +0300

    Task 4 malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created 
+ * free_grid - frees a 2 dimensional grid previously created
  * by alloc_grid function.
  * @grid: multidimensional array of integers.
  * @height: height of the grid.
