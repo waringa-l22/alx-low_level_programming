58a38c28bea5f07053c2bfc57cc25547a7ac57c4
waringa-l22 <lwaringa22@gmail.com>
2022-10-04 04:52:53 +0300

    Main file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: alx
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -17,4 +10,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* MAIN_H *
+#endif
